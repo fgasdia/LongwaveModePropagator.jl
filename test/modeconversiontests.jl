8c2ee4671b02558d765b48e29aad05eb7b336b8b
fgasdia <fgasdia@noreply.github.com>
2020-06-10 20:59:59 -0600

    about to separate transmitter height gain terms

--- test/modeconversiontests.jl
+++ test/modeconversiontests.jl
@@ -243,65 +243,6 @@ end
 
 
 
-function modeconversion(previous_wavefields::Wavefields{T},
-                        wavefields::Wavefields{T}, adjwavefields::Wavefields{T}) where {T}
-
-    @assert LWMS.numheights(previous_wavefields) == LWMS.numheights(wavefields)
-    zs = LWMS.heights(wavefields)
-
-    # TODO: Assuming `length(zs)` is always the same, we can reuse `product` b/t calls
-    product = Vector{ComplexF64}(undef, LWMS.numheights(wavefields))
-
-    modes = LWMS.eigenangles(wavefields)
-    adjmodes = LWMS.eigenangles(adjwavefields)
-    prevmodes = LWMS.eigenangles(previous_wavefields)
-
-    modes == adjmodes || @warn "Full mode conversion physics assumes adjoint
-        wavefields are calculated for eigenangles of the original waveguide."
-
-    nummodes = length(modes)
-    numadjmodes = length(adjmodes)
-    numprevmodes = length(prevmodes)
-
-    # Calculate _inverse_ normalization terms, 1/N
-    # the inverse is more efficient because we would otherwise repeatedly divide
-    # by N later
-    invN = Vector{ComplexF64}(undef, nummodes)
-    for m in eachindex(modes)
-        for i in eachindex(zs)
-            @inbounds f = wavefields[m][i][SVector(2,3,5,6)]  # Ey, Ez, Hy, Hz
-            @inbounds g = SVector{4,ComplexF64}(adjwavefields[m][i][6], -adjwavefields[m][i][5], -adjwavefields[m][i][3], adjwavefields[m][i][2])  # Hz, -Hy, -Ez, Ey
-            product[i] = transpose(g)*f
-        end
-
-        N = LWMS.integrate(zs, product, LWMS.RombergEven(ROMBERG_ACCURACY))
-        invN[m] = inv(N)
-    end
-
-    # TODO: because nummodes = numadjmodes, we don't need to have two sets of loops
-    # we can just have a scalar N, but will need to flip dimensions of `a` to
-    # write down column first. Also, we would then need two product vectors, which
-    # are relatively large
-
-    # `a` is total conversion
-    a = Matrix{ComplexF64}(undef, numadjmodes, numprevmodes)
-    for k in eachindex(prevmodes)
-        for m in eachindex(adjmodes)
-            for i in eachindex(zs)
-                @inbounds f = previous_wavefields[k][i][SVector(2,3,5,6)]  # Ey, Ez, Hy, Hz
-                @inbounds g = SVector{4,ComplexF64}(adjwavefields[m][i][6], -adjwavefields[m][i][5], -adjwavefields[m][i][3], adjwavefields[m][i][2])  # Hz, -Hy, -Ez, Ey
-                product[i] = transpose(g)*f
-            end
-            I = LWMS.integrate(zs, product, LWMS.RombergEven(ROMBERG_ACCURACY))
-            @inbounds a[m,k] = I*invN[m]
-        end
-    end
-
-    return a
-end
-
-
-
 
 
 
