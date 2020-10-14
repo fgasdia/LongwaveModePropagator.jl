9ba580ea0486d933055bfdc486b1f2cb5ec0be09
Forrest Gasdia <fgasdia@users.noreply.github.com>
2020-10-13 22:54:52 -0600

    Append .json to auto file

--- src/LongwaveModeSolver.jl
+++ src/LongwaveModeSolver.jl
@@ -252,7 +252,7 @@ function bpm(file::AbstractString; incrementalwrite=false, append=false)
 
     s = parse(file)
     if incrementalwrite
-        output = buildrunsave(outfile, s, append=append)
+        output = buildrunsave(outfile*"_bpm.json", s, append=append)
     else
         output = buildrun(s)
 
