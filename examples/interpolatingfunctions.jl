6c7f8501d905ba8395ee923e6951dce8939b4cbb
Forrest Gasdia <17058025+fgasdia@users.noreply.github.com>
2021-04-16 21:09:03 -0600

    Completed interpolators example

--- examples/interpolatingfunctions.jl
+++ examples/interpolatingfunctions.jl
@@ -37,12 +37,13 @@ using Plots, Distances
 
 using Interpolations, NormalHermiteSplines
 using Interp
+nothing #hide
 
 # Here's the discrete profile data. Some interpolators will benefit from random sample
 # points, but real density data will almost always be on a grid.
 
 zs = 0:500:110e3
-Ne = waitprofile.(zs, 75, 0.32; cutoff_low=40e3)
+Ne = waitprofile.(zs, 75, 0.32; cutoff_low=40e3);
 
 # Let's construct the interpolators.
 
@@ -51,9 +52,9 @@ cubic_itp = CubicSplineInterpolation(zs, Ne)
 
 spline = prepare(collect(zs), RK_H1())
 spline = construct(spline, Ne)
-hermite_itp(z) = evaluate(spline, collect(z))
+hermite_itp(z) = evaluate_one(spline, z)
 
-pchip_itp = pchip(zs, Ne)
+pchip_itp = pchip(zs, Ne);
 
 
 # (a spline built with RK_H0 kernel is a continuous function,
@@ -65,8 +66,8 @@ Ne_fine = waitprofile.(zs_fine, 75, 0.32; cutoff_low=40e3)
 
 linear_fine = linear_itp.(zs_fine)
 cubic_fine = cubic_itp.(zs_fine)
-hermite_fine = hermite_itp(zs_fine)
-pchip_fine = pchip_itp.(zs_fine)
+hermite_fine = hermite_itp.(zs_fine)
+pchip_fine = pchip_itp.(zs_fine);
 
 # The profiles are compared using percentage difference relative to the true profile.
 
@@ -76,7 +77,7 @@ dNe = cmp(Ne_fine, Ne_fine)
 dlinear = cmp(linear_fine, Ne_fine)
 dcubic = cmp(cubic_fine, Ne_fine)
 dhermite = cmp(hermite_fine, Ne_fine)
-dpchip = cmp(pchip_fine, Ne_fine)
+dpchip = cmp(pchip_fine, Ne_fine);
 
 # To plot densities with a log scale, we set values less than 0.1 to NaN.
 
@@ -103,8 +104,6 @@ plot(p1, p2, p3, layout=(1,3), size=(800,400))
 # 
 # The total RMS differences between each interpolator and the truth are:
 
-interpolators = 
-
 for (n, v) in ("linear"=>linear_fine, "cubic"=>cubic_fine, "hermite"=>hermite_fine, "pchip"=>pchip_fine)
     @printf("%s: %.3e\n",n, rmsd(v, Ne_fine))
 end
@@ -112,16 +111,18 @@ end
 # ## Propagation results
 # 
 # Let's compare propagation results and performance with each of these interpolators.
-
-
-# Here are the `Species` for each interpolator.
+# Unfortunately, as of `v0.5.1` of NormalHermiteSplines.jl, the implementation of the
+# package isn't efficient (see #3)[https://github.com/IgorKohan/NormalHermiteSplines.jl/issues/3]
+# and because `hermite_itp` would be called millions of times in `propagate`, it is
+# prohibitively slow.
+# 
+# Here are the `Species` for each of the other interpolators.
 # They will all use the analytic [`electroncollisionfrequency`](@ref).
 
 interpolators = (
     "truth" => z->waitprofile(z, 75, 0.32; cutoff_low=40e3),
     "linear"=> linear_itp,
     "cubic" => cubic_itp,
-    "hermite" => hermite_itp,
     "pchip" => pchip_itp
 )
 
@@ -132,20 +133,53 @@ function propagateitp(interpolators)
     tx = Transmitter(24e3)
     rx = GroundSampler(0:5e3:3000e3, Fields.Ez)
 
-    results = Dict{String,NTuple{3,Float64}}()
+    results = Dict{String,Tuple{Float64,Vector{Float64},Vector{Float64}}}()
     for (n, itp) in interpolators
         species = Species(QE, ME, itp, electroncollisionfrequency)
-        waveguide = HomogeneousWaveguide(bfield, electrons, ground)
+        waveguide = HomogeneousWaveguide(bfield, species, ground)
+        t0 = time()
         E, amp, phase = propagate(waveguide, tx, rx)
+        runtime = time() - t0
+        results[n] = (runtime, amp, phase)
     end
     return results
 end
 
+propagateitp(interpolators);  # warmup
+results = propagateitp(interpolators);
 
-# Timing can vary when run by github actions build, but normalized results from a local run are here:
+# First let's evaluate the propagation results.
 
-p1 = plot(rx.distance/1000, ae, label="electrons", ylabel="Amplitude (dB μV/m)")
-plot!(p1, rx.distance/1000, aei, label="electrons & ions")
-p2 = plot(rx.distance/1000, aei-ae,
-    ylims=(-0.5, 0.5), xlabel="Range (km)", ylabel="Δ", legend=false)
+d = 0:5:3000
+p1 = plot(ylabel="Amplitude (dB μV/m)")
+p2 = plot(xlabel="Range (km)", ylims=(-0.1, 0.1), ylabel="Δ", legend=false)
+for (n, v) in results
+    plot!(p1, d, v[2], label=n)
+    plot!(p2, d, v[2]-results["truth"][2])
+end
 plot(p1, p2, layout=grid(2,1,heights=[0.7, 0.3]))
+#md savefig("interpolatingfunctions_amp.png"); nothing # hide
+#md # ![](interpolatingfunctions_amp.png)
+
+p1 = plot(ylabel="Phase (deg)")
+p2 = plot(xlabel="Range (km)", ylims=(-1, 1), ylabel="Δ", legend=false)
+for (n, v) in results
+    plot!(p1, d, rad2deg.(v[3]), label=n)
+    plot!(p2, d, rad2deg.(v[3])-rad2deg.(results["truth"][3]))
+end
+plot(p1, p2, layout=grid(2,1,heights=[0.7, 0.3]))
+#md savefig("interpolatingfunctions_phase.png"); nothing # hide
+#md # ![](interpolatingfunctions_phase.png)
+
+# The amplitude and phase for each of the interpolators matches the true exponential profile
+# extremely closely.
+#  
+# Timing can vary when run by GitHub to build the documentation, so results here are from a
+# local run:
+# 
+# | Interpolator | Runtime relative to `truth` |
+# | ------------ | --------------------------- |
+# | truth        |              1              |
+# | linear       |             2.17            |
+# | cubic        |             1.02            |
+# | pchip        |             1.08            |
